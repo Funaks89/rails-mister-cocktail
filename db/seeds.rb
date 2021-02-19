781c46cfb974afb6e207e9a2e82a3116b165c0d3
Ken Funakoshi <funaks@live.com>
2021-02-19 12:08:03 +0900

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+puts 'destroying'
+Ingredient.destroy_all
+Cocktail.destroy_all
+puts 'destroyed'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+user['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+  puts "ingredient created"
+end
+
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+Cocktail.create(name: "High Ball")
+Cocktail.create(name: "Screw Driver")
+Cocktail.create(name: "Catalina Wine Mixer")
+Cocktail.create(name: "Brain tumor")
+Cocktail.create(name: "Some chick drink")
